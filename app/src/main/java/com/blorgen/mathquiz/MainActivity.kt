d87e2e8e1d6a28569915e7c2981f9010ff426cc1
Blorgen <39773024+Blorgen@users.noreply.github.com>
2021-01-20 23:40:34 +0400

    Initial commit

---
+++ app/src/main/java/com/blorgen/mathquiz/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.blorgen.mathquiz
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            val intent = Intent(this, PlayActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.action_settings -> {
+
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
